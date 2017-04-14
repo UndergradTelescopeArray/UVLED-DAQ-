528f7d8c2c16dd159854dbf1d83ee54e2ad588d0
GenericCode <rstahulak@redrocksw.com>
2017-04-14 16:44:09 -0600

    Improved to actually work; should find residuals just fine

--- RootFit.cpp
+++ RootFit.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <cstddef>
 
 int fit() {
   std::vector<double> flux, fluxErr, ampl, freq, dist;
@@ -48,15 +49,25 @@ int fit() {
       column++;
       tokBuffer = std::strtok(NULL,",");
     }//End while
-    
-    TCanvas *c1 = new TCanvas("c1","Percent residual vs Distance",200,10,700,500);
-    TProfile *resProf = new TProfile("resProf", "Percent residual vs Distance",100,0,2);
-    for(int i = 0; i<flux.size(); i++ ) {
-      resProf->Fill( dist[i], (gnuFit(dist[i],ampl[i],freq[i])-flux[i])/flux[i] );
-    }
-    resProf->Draw();
     //std::cout << flux[i] << "," << fluxErr[i] << "," << dist[i] << "," << ampl[i] << "," << freq[i] << "\n";
+  }//End other while
+
+  TProfile *resProfX = new TProfile("resProfX", "Percent residual vs Distance",100,0,1);
+  TProfile *resProfA = new TProfile("resProfA", "Percent residual vs Amplitude",100,60,170);
+  TProfile *resProfV = new TProfile("resProfV", "Percent residual vs Frequency",100,0,75000);
+
+  for(int i = 0; i<flux.size(); i++ ) {
+    resProfX->Fill( dist[i], 100*(gnuFit(dist[i],ampl[i],freq[i])-flux[i])/flux[i] );
+    resProfA->Fill( ampl[i], 100*(gnuFit(dist[i],ampl[i],freq[i])-flux[i])/flux[i] );
+    resProfV->Fill( freq[i], 100*(gnuFit(dist[i],ampl[i],freq[i])-flux[i])/flux[i] );
   }
+
+TCanvas *c1 = new TCanvas("c1","Percent residual vs Distance",700,700);
+  resProfX->Draw();
+TCanvas *c2 = new TCanvas("c2","Percent residual vs Amplitude",700,700);
+  resProfA->Draw();
+TCanvas *c3 = new TCanvas("c3","Percent residual vs Frequency",700,700);
+  resProfV->Draw();
 }
 
 double gnuFit(double x, double amplitude, double frequency) {
@@ -65,5 +76,16 @@ double gnuFit(double x, double amplitude, double frequency) {
   const double c = 160.165;
   const double d = -0.0176522;
   double f = (a*frequency+b)*(1-exp(-amplitude/c))/pow(x-d,2);
+  cout << f << "\n";
+  return f;
+}
+
+double gnuFitOffset(double x, double amplitude, double frequency) {
+  const double a = -0.00134129;
+  const double b = 692.699;
+  const double c = 160.165;
+  const double d = -0.0176522;
+  double f = (a*frequency+b)*(1-exp(-amplitude/c))/pow(x-d,2);
+  cout << f << "\n";
   return f;
 }
